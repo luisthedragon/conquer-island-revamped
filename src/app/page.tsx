28c368f9a56cf51d462a4ff527342acf2d64e304
Luis <lgeldragondorado@gmail.com>
2024-05-05 13:35:16 -0500

    Add board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,64 @@
-import Link from "next/link";
+"use client";
+import { useState, useEffect } from "react";
+import seedrandom from "seedrandom";
 
 export default function HomePage() {
+  const MAX_SEED_VALUE = 1000000000;
+  const nrows = 3;
+  const ncols = 3;
+  const [board, setBoard] = useState([[0]]);
+
+  useEffect(() => {
+    const seed = Math.floor(Math.random() * MAX_SEED_VALUE);
+    const rng = seedrandom(seed + "");
+    let new_board = [];
+    for (let i = 0; i < nrows; i++) {
+      new_board.push(
+        new Array(ncols).fill(1).map(() => (rng() >= 0.5 ? 1 : 0)),
+      );
+    }
+    setBoard(new_board);
+  }, []); // Dependency array ensures this effect runs only once (TODO: Check if there's a better way of doing this or this is the standard)
+
+  console.log(board);
+
+  const getImageUrl = (cellValue: number) => {
+    return cellValue === 1
+      ? "sprites/squared/dog.jpg"
+      : "sprites/squared/lion.jpg";
+  };
+
+  const handleClick = (row_idx: number, col_idx: number) => {
+    console.log(`row: ${row_idx} col: ${col_idx}`);
+    console.log(board);
+    // Implement your logic for handling clicks here
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+        <div className="home">
+          <h2>You are in the dogs&apos; team.</h2>
+          <h3>
+            <b>Mission:</b> Defeat the lions by dominating the island.
+          </h3>
+        </div>
+        <div className="">
+          {board.map((row, rowIndex) => (
+            <div key={rowIndex} className="flex flex-row">
+              {row.map((cell, columnIndex) => (
+                <div
+                  key={columnIndex}
+                  onClick={() => handleClick(rowIndex, columnIndex)}
+                  className={`flex h-24 w-24 cursor-pointer items-center justify-center p-2`}
+                  style={{
+                    backgroundImage: `url(${getImageUrl(cell)})`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              ))}
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+          ))}
         </div>
       </div>
     </main>
