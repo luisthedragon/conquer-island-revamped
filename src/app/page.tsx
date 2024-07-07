a42ac975654262d8c8631fe8803476020ccdc570
luisthedragon <lgeldragondorado@gmail.com>
2024-07-06 20:48:22 -0500

    fix Type error: Argument of type 'string' is not assignable to parameter of type 'number'.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function HomePage() {
   // https://stackoverflow.com/a/175787
   const isNumeric = (str: string) => {
     if (typeof str != "string") return false // we only process strings!  
-    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+    return !isNaN(parseInt(str)) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
            !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
   }
 
